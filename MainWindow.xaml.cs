2dd914ad0b7092380fdd8045367d00ef63013140
danie <danie@DESKTOP-8LKD41C>
2024-10-14 08:38:39 +0300

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using EPubReader.Models;
+using Microsoft.Win32;
+using Newtonsoft.Json;
+using System.IO;
+using System.Net.Http;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+//using System.Windows.Navigation;
+//using System.Windows.Shapes;
+using VersOne.Epub;
+using static System.Reflection.Metadata.BlobBuilder;
+
+namespace EPubReader
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
